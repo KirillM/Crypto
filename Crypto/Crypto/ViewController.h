2cb912a5e98f90eaf3b56ec5420ae38936d4bf12
Kirill Mezrin <mezrink@gmail.com>
2018-03-31 17:40:16 +0500

    Initial commit

---
+++ Crypto/Crypto/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Crypto
+//
+//  Created by Kirill Mezrin on 31/03/2018.
+//  Copyright © 2018 Kirill Mezrin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Crypto/Crypto/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Crypto
+//
+//  Created by Kirill Mezrin on 31/03/2018.
+//  Copyright © 2018 Kirill Mezrin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
