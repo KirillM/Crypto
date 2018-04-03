b570494d3a1034e5caeffb331ba4964215d64996
Kirill Mezrin <mezrink@gmail.com>
2018-04-03 12:16:15 +0500

    Main error show

--- Crypto/Crypto/Controllers/MainViewController.m
+++ Crypto/Crypto/Controllers/MainViewController.m
@@ -14,6 +14,7 @@
 #import "AddCurrencyViewController.h"
 #import "MainCurrencyTableViewCell.h"
 #import "CurrencyObject.h"
+#import "ABTopAlertView.h"
 
 static NSString *const kAddCurrencyViewControllerSegue = @"kAddCurrencyViewControllerSegue";
 static NSString *kUserDefaultsExchangeRatesKey = @"UserDefaultsExchangeRates";
@@ -64,6 +65,7 @@ static NSString *kUserDefaultsExchangeRatesKey = @"UserDefaultsExchangeRates";
         weakSelf.amountBTCTextField.text = [NSString stringWithFormat:@"%f", [self calculate]];
     }).catch (^(NSError *error) {
         [error log:NSStringFromClass(self.class) method:NSStringFromSelector(_cmd)];
+        [[ABTopAlertView shared] showError:error.localizedDescription];
     });
 }
 
